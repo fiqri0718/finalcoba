2efcbfbadd1a09418794b2f76ad2054633329f60
Maulana Fiqri Nurul Fawaid <maulanafiqri310@gmail.com>
2023-05-17 20:05:36 +0700

    new

---
+++ html-final-project-library-app-v1/deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Maulana Fiqri Nurul Fawaid';
+
+const studentId = 'FE4486647';
+
+const NetlifyDeployUrl = 'https://marvelous-tapioca-718d67.netlify.app/';
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
